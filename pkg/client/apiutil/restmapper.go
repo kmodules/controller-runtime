5bdfc22125ce3208b6b8155ba8fc441d61edb90e
Tamal Saha <tamal@appscode.com>
2023-10-22 19:14:23 -0700

    Always discover all versions for an apiGroup
    Fixes https://github.com/kubernetes-sigs/controller-runtime/issues/2556
    
    Signed-off-by: Tamal Saha <tamal@appscode.com>

--- pkg/client/apiutil/restmapper.go
+++ pkg/client/apiutil/restmapper.go
@@ -65,7 +65,7 @@ type mapper struct {
 func (m *mapper) KindFor(resource schema.GroupVersionResource) (schema.GroupVersionKind, error) {
 	res, err := m.getMapper().KindFor(resource)
 	if meta.IsNoMatchError(err) {
-		if err := m.addKnownGroupAndReload(resource.Group, resource.Version); err != nil {
+		if err := m.addKnownGroupAndReload(resource.Group); err != nil {
 			return schema.GroupVersionKind{}, err
 		}
 		res, err = m.getMapper().KindFor(resource)
@@ -78,7 +78,7 @@ func (m *mapper) KindFor(resource schema.GroupVersionResource) (schema.GroupVers
 func (m *mapper) KindsFor(resource schema.GroupVersionResource) ([]schema.GroupVersionKind, error) {
 	res, err := m.getMapper().KindsFor(resource)
 	if meta.IsNoMatchError(err) {
-		if err := m.addKnownGroupAndReload(resource.Group, resource.Version); err != nil {
+		if err := m.addKnownGroupAndReload(resource.Group); err != nil {
 			return nil, err
 		}
 		res, err = m.getMapper().KindsFor(resource)
@@ -91,7 +91,7 @@ func (m *mapper) KindsFor(resource schema.GroupVersionResource) ([]schema.GroupV
 func (m *mapper) ResourceFor(input schema.GroupVersionResource) (schema.GroupVersionResource, error) {
 	res, err := m.getMapper().ResourceFor(input)
 	if meta.IsNoMatchError(err) {
-		if err := m.addKnownGroupAndReload(input.Group, input.Version); err != nil {
+		if err := m.addKnownGroupAndReload(input.Group); err != nil {
 			return schema.GroupVersionResource{}, err
 		}
 		res, err = m.getMapper().ResourceFor(input)
@@ -104,7 +104,7 @@ func (m *mapper) ResourceFor(input schema.GroupVersionResource) (schema.GroupVer
 func (m *mapper) ResourcesFor(input schema.GroupVersionResource) ([]schema.GroupVersionResource, error) {
 	res, err := m.getMapper().ResourcesFor(input)
 	if meta.IsNoMatchError(err) {
-		if err := m.addKnownGroupAndReload(input.Group, input.Version); err != nil {
+		if err := m.addKnownGroupAndReload(input.Group); err != nil {
 			return nil, err
 		}
 		res, err = m.getMapper().ResourcesFor(input)
@@ -117,7 +117,7 @@ func (m *mapper) ResourcesFor(input schema.GroupVersionResource) ([]schema.Group
 func (m *mapper) RESTMapping(gk schema.GroupKind, versions ...string) (*meta.RESTMapping, error) {
 	res, err := m.getMapper().RESTMapping(gk, versions...)
 	if meta.IsNoMatchError(err) {
-		if err := m.addKnownGroupAndReload(gk.Group, versions...); err != nil {
+		if err := m.addKnownGroupAndReload(gk.Group); err != nil {
 			return nil, err
 		}
 		res, err = m.getMapper().RESTMapping(gk, versions...)
@@ -130,7 +130,7 @@ func (m *mapper) RESTMapping(gk schema.GroupKind, versions ...string) (*meta.RES
 func (m *mapper) RESTMappings(gk schema.GroupKind, versions ...string) ([]*meta.RESTMapping, error) {
 	res, err := m.getMapper().RESTMappings(gk, versions...)
 	if meta.IsNoMatchError(err) {
-		if err := m.addKnownGroupAndReload(gk.Group, versions...); err != nil {
+		if err := m.addKnownGroupAndReload(gk.Group); err != nil {
 			return nil, err
 		}
 		res, err = m.getMapper().RESTMappings(gk, versions...)
@@ -151,26 +151,19 @@ func (m *mapper) getMapper() meta.RESTMapper {
 }
 
 // addKnownGroupAndReload reloads the mapper with updated information about missing API group.
-// versions can be specified for partial updates, for instance for v1beta1 version only.
-func (m *mapper) addKnownGroupAndReload(groupName string, versions ...string) error {
-	// versions will here be [""] if the forwarded Version value of
-	// GroupVersionResource (in calling method) was not specified.
-	if len(versions) == 1 && versions[0] == "" {
-		versions = nil
-	}
-
-	// If no specific versions are set by user, we will scan all available ones for the API group.
+func (m *mapper) addKnownGroupAndReload(groupName string) error {
+	// We will scan all available ones for the API group.
 	// This operation requires 2 requests: /api and /apis, but only once. For all subsequent calls
 	// this data will be taken from cache.
-	if len(versions) == 0 {
-		apiGroup, err := m.findAPIGroupByName(groupName)
-		if err != nil {
-			return err
-		}
-		if apiGroup != nil {
-			for _, version := range apiGroup.Versions {
-				versions = append(versions, version.Version)
-			}
+
+	var versions []string
+	apiGroup, err := m.findAPIGroupByName(groupName)
+	if err != nil {
+		return err
+	}
+	if apiGroup != nil {
+		for _, version := range apiGroup.Versions {
+			versions = append(versions, version.Version)
 		}
 	}
 
